fcf04fc57ecab06b8988fbb17da57f882da4f7cd
Alejandro-garcia-gutierrez <82551017+Alejandro-garcia-gutierrez@users.noreply.github.com>
2021-04-25 19:57:48 -0500

    Add files via upload

---
+++ Programa 1.c
@@ -0,0 +1,12 @@
+#include
+
+int main() {
+    /* García  Gutierrez Alejandro
+    Equipo Los ocho tristes burros
+    21 abr 21
+    Programa 1
+    */
+
+   printf ("Hola mundo");
+   return 0;
+}
